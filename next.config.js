706e099f954af4643494ffb871e853917ba8a4bb
Luis Juárez <luis.juarez@contrastsecurity.com>
2022-01-29 18:09:03 -0600

    BLOG-1 Initial Commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['pbs.twimg.com']
+  }
+})
+
+module.exports = {
+  assetPrefix: isProd ? '/luis/' : ''
+}
